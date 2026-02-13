99f539629fa72fc591678a6d781d810d448712f5
supunpriyanath333 <www.supunbro12@gmail.com>
2026-02-13 23:56:40 +0530

    added themes

---
+++ src/constants/theme.js
@@ -0,0 +1,8 @@
+// src/constants/theme.js
+export const COLORS = {
+  background: '#050a08', // Dark green-black
+  surface: '#0a120e',    // Slightly lighter dark green
+  primary: '#10ac84',    // The bright green from your badges
+  text: '#ffffff',
+  secondaryText: '#888888',
+};
